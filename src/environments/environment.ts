edf7ea1234450f36ba9fd89b843f5dc2ae5909e9
jhojanhernandezy24 <jhojanhernandezy24@outlook.es>
2023-09-19 06:54:20 -0500

    endspoints

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiURL: 'https://localhost:44355/',
+  getByIdUserEndpoint: 'User/GetById/',
+  deleteUserEndpoint: 'User/Delete/',
+  getAllUserEndpoint: 'User/GetAllUsers',
+  sigUpEndpoint: 'User/SignUp',
+  getUserTypeEndpoint: 'UserType/GetAllType',
+
 };
 
 /*
